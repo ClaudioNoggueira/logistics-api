c4c23a5556f363b494245f6d872f8ac7d9623d2c
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-05-26 12:09:58 -0300

    Add Customer model

---
+++ src/main/java/com/claudionogueira/logisticsproject/domain/models/Customer.java
@@ -0,0 +1,32 @@
+package com.claudionogueira.logisticsproject.domain.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Customer {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "phone")
+	private String phone;
+}
