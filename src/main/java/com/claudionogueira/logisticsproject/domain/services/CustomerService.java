9daa321f0992e0a64a825b61c314af126c25a6bb
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-05-27 11:42:40 -0300

    Handle null and blank exception in update customer method

--- src/main/java/com/claudionogueira/logisticsproject/domain/services/CustomerService.java
+++ src/main/java/com/claudionogueira/logisticsproject/domain/services/CustomerService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.claudionogueira.logisticsproject.domain.exceptions.DomainException;
 import com.claudionogueira.logisticsproject.domain.exceptions.ObjectNotFoundException;
 import com.claudionogueira.logisticsproject.domain.models.Customer;
 import com.claudionogueira.logisticsproject.domain.repositories.CustomerRepo;
@@ -31,15 +32,28 @@ public class CustomerService implements ICustomerService {
 
 	@Override
 	public void add(Customer entity) {
+		boolean emailExists = repo.findByEmail(entity.getEmail()).stream()
+				.anyMatch(existingCustomer -> !existingCustomer.equals(entity));
+
+		if (emailExists)
+			throw new DomainException("Email is already in use.");
+
 		repo.save(entity);
 	}
 
 	@Override
 	public void update(Long id, Customer entity) {
 		Customer objToBeUpdated = this.findById(id);
-		objToBeUpdated.setName(entity.getName());
-		objToBeUpdated.setEmail(entity.getEmail());
-		objToBeUpdated.setPhone(entity.getPhone());
+
+		if (entity.getName() != null && !entity.getName().isBlank())
+			objToBeUpdated.setName(entity.getName());
+
+		if (entity.getEmail() != null && !entity.getEmail().isBlank())
+			objToBeUpdated.setEmail(entity.getEmail());
+
+		if (entity.getPhone() != null && !entity.getPhone().isBlank())
+			objToBeUpdated.setPhone(entity.getPhone());
+
 		repo.save(objToBeUpdated);
 	}
 
