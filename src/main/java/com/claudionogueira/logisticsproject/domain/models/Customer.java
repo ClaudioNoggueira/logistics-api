e6e0461a468b8f41ab9fb58bf9ef993aa7625e28
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-05-27 10:14:01 -0300

    Add bean validation annotations

--- src/main/java/com/claudionogueira/logisticsproject/domain/models/Customer.java
+++ src/main/java/com/claudionogueira/logisticsproject/domain/models/Customer.java
@@ -7,6 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Customer implements Serializable {
@@ -16,12 +19,19 @@ public class Customer implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@NotBlank
+	@Size(max = 60)
 	@Column(name = "name")
 	private String name;
 
+	@NotBlank
+	@Email
+	@Size(max = 255)
 	@Column(name = "email")
 	private String email;
 
+	@NotBlank
+	@Size(max = 20)
 	@Column(name = "phone")
 	private String phone;
 
