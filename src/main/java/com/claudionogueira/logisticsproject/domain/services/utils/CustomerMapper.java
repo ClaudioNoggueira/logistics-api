7a840d9390b2d9840e4e505dd0724903b97b7f78
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-05-28 13:20:45 -0300

    Add customer mapping from input to entity

--- src/main/java/com/claudionogueira/logisticsproject/domain/services/utils/CustomerMapper.java
+++ src/main/java/com/claudionogueira/logisticsproject/domain/services/utils/CustomerMapper.java
@@ -7,6 +7,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 import com.claudionogueira.logisticsproject.api.dtos.CustomerDTO;
+import com.claudionogueira.logisticsproject.api.dtos.inputs.CustomerInput;
 import com.claudionogueira.logisticsproject.domain.models.Customer;
 
 @Component
@@ -25,6 +26,10 @@ public class CustomerMapper {
 
 	public Customer toEntity(CustomerDTO dto) {
 		return mapper.map(dto, Customer.class);
+	}
+
+	public Customer fromInputToEntity(CustomerInput input) {
+		return mapper.map(input, Customer.class);
 	}
 
 	public List<CustomerDTO> toListDTO(List<Customer> list) {
