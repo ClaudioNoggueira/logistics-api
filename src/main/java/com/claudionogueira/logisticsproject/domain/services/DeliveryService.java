fb20ae80ee4cf238e597085f78198d9946118c80
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-05-28 09:50:36 -0300

    Refactor use delivery mapper

--- src/main/java/com/claudionogueira/logisticsproject/domain/services/DeliveryService.java
+++ src/main/java/com/claudionogueira/logisticsproject/domain/services/DeliveryService.java
@@ -13,16 +13,19 @@ import com.claudionogueira.logisticsproject.domain.models.Delivery;
 import com.claudionogueira.logisticsproject.domain.models.enums.DeliveryStatus;
 import com.claudionogueira.logisticsproject.domain.repositories.DeliveryRepo;
 import com.claudionogueira.logisticsproject.domain.services.interfaces.IDeliveryService;
+import com.claudionogueira.logisticsproject.domain.services.utils.DeliveryMapper;
 
 @Service
 public class DeliveryService implements IDeliveryService {
 
 	private final DeliveryRepo deliveryRepo;
 	private final CustomerService customerService;
+	private final DeliveryMapper deliveryMapper;
 
-	public DeliveryService(DeliveryRepo deliveryRepo, CustomerService customerService) {
+	public DeliveryService(DeliveryRepo deliveryRepo, CustomerService customerService, DeliveryMapper deliveryMapper) {
 		this.deliveryRepo = deliveryRepo;
 		this.customerService = customerService;
+		this.deliveryMapper = deliveryMapper;
 	}
 
 	@Transactional
@@ -43,7 +46,7 @@ public class DeliveryService implements IDeliveryService {
 	@Transactional
 	@Override
 	public DeliveryDTO findById(Long id) {
-		return deliveryRepo.findById(id).map(delivery -> new DeliveryDTO(delivery))
+		return deliveryRepo.findById(id).map(delivery -> deliveryMapper.toDTO(delivery))
 				.orElseThrow(() -> new ObjectNotFoundException("Delivery with ID: '" + id + "' not found."));
 	}
 
